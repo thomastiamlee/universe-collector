2ecea795a5772e1b126c5b6b43e26b0b3a5fe840
Thomas <thomasjamestiamlee@gmail.com>
2016-11-26 02:35:34 +0800

    Initial user interace.

--- main.js
+++ main.js
@@ -8,9 +8,16 @@ var mainWindow = null;
 
 app.on('ready', function() {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    resizable: false
+    width: 1024,
+    height: 700,
+    resizable: false,
+    show: false,
+  });
+  // mainWindow.openDevTools();
+
+  mainWindow.setMenu(null);
+  mainWindow.once("ready-to-show", function() {
+    mainWindow.show();
   });
 
   mainWindow.loadURL('file://' + __dirname + '/app/index.html');
