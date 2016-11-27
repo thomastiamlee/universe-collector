0a9b6b9d00a8bc9e7f9673a36811249da8d9c02c
Thomas <thomasjamestiamlee@gmail.com>
2016-11-27 22:14:10 +0800

    Welcome screen for getting details of the subject.

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 'use strict';
 
 const app = require('electron').app;
-const BrowserWindow = require('electron').BrowserWindow
+const BrowserWindow = require('electron').BrowserWindow;
+const ipcMain = require("electron").ipcMain;
 const path = require('path');
 
 var mainWindow = null;
+var welcomeWindow = null;
 
 app.on('ready', function() {
+  welcomeWindow = new BrowserWindow({
+    width: 400,
+    height: 200,
+    resizable: false,
+    show: false
+  });
+
   mainWindow = new BrowserWindow({
     width: 1024,
     height: 700,
@@ -14,11 +23,24 @@ app.on('ready', function() {
     show: false,
   });
   mainWindow.openDevTools();
-
   mainWindow.setMenu(null);
-  mainWindow.once("ready-to-show", function() {
+  welcomeWindow.openDevTools();
+  welcomeWindow.setMenu(null);
+
+  welcomeWindow.once("ready-to-show", function() {
+    welcomeWindow.show();
+  });
+
+  ipcMain.on("details-submitted", function(event, data) {
+    welcomeWindow.close();
+    mainWindow.webContents.send("create-details", data);
     mainWindow.show();
   });
 
+  /*mainWindow.once("ready-to-show", function() {
+    mainWindow.show();
+  });*/
+
+  welcomeWindow.loadURL('file://' + __dirname + '/app/welcome.html');
   mainWindow.loadURL('file://' + __dirname + '/app/index.html');
 });
